d73778b127c125c6c0973e8c2ae3a3d62ff1d43e
Ryan Forgie <ryan.j.forgie@gmail.com>
2017-10-14 13:49:49 +0100

    add hero test

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,2 +1,15 @@
 var assert = require("assert")
 var Hero = require("../hero")
+
+describe("Hero", function () {
+  var hero1
+
+
+  beforeEach(function () {
+    hero1 = new Hero("Inigo Montoya", 20, "cake")
+  })
+
+  it("should have a name", function () {
+    assert.strictEqual(hero1.name, "Inigo Montoya")
+  })
+})
