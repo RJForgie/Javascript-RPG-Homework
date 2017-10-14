c74b322c637b374229c1efe850e7dacdd54e1466
Ryan Forgie <ryan.j.forgie@gmail.com>
2017-10-14 14:33:43 +0100

    Add completed function to task, refactored difficulty and urgency to be ints

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -5,18 +5,31 @@ describe("Task", function () {
   var task1
 
   beforeEach(function () {
-    task1 = new Task("beginner", "low", 5)
+    task1 = new Task("avenge father", 5, 3, 100)
+  })
+
+  it("should have a description", function () {
+    assert.strictEqual(task1.description, "avenge father")
   })
 
   it("should have a difficulty level", function () {
-    assert.strictEqual(task1.difficulty, "beginner")
+    assert.strictEqual(task1.difficulty, 5)
   })
 
   it("should have an urgency level", function () {
-    assert.strictEqual(task1.urgency, "low")
+    assert.strictEqual(task1.urgency, 3)
   })
 
   it("should have a reward amount", function () {
-    assert.strictEqual(task1.rewardAmount, 5)
+    assert.strictEqual(task1.rewardAmount, 100)
+  })
+
+  it("should start as not completed", function () {
+    assert.strictEqual(task1.completed, false)
+  })
+
+  it("can be completed", function () {
+    task1.completeTask()
+    assert.strictEqual(task1.completed, true)
   })
 })
--- task.js
+++ task.js
@@ -1,8 +1,15 @@
-Task = function (difficulty, urgency, rewardAmount) {
-  this.difficulty = difficulty
-  this.urgency = urgency
+Task = function (description, difficulty, urgency, rewardAmount) {
+  this.description = description
+  this.difficulty = difficulty   //Difficulty scored 1-5, 5 being most difficult
+  this.urgency = urgency //Urgency is rated 1-3, 3 being most urgent
   this.rewardAmount = rewardAmount
   this.completed = false
+}
+
+Task.prototype = {
+  completeTask: function () {
+    this.completed = true
+  }
 }
 
 module.exports = Task
